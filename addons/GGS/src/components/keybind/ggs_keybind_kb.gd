4529a8d0a8a0bff7dbfdad34b67dd3e5d0836937
Kylogias <45217130+Kylogias@users.noreply.github.com>
2021-05-29 09:55:13 -0400

    Add Mouse Support - Components

--- addons/GGS/src/components/keybind/ggs_keybind_kb.gd
+++ addons/GGS/src/components/keybind/ggs_keybind_kb.gd
@@ -5,6 +5,9 @@ var script_instance: Object
 
 # Resources
 onready var ConfirmPopup: PackedScene = preload("KeybindConfirm.tscn")
+const mouse_btn_names := {"1": "Left Mouse Button", "2": "Right Mouse Button", "3": "Middle Mouse Button",
+							"4": "Mouse Wheel Up", "5": "Mouse Wheel Down", "6": "Mouse Wheel Left",
+							"7": "Mouse Wheel Right", "8": "Mouse Extra Button 1", "9": "Mouse Extra Button 2"}
 
 
 func _ready() -> void:
@@ -28,8 +31,13 @@ func _ready() -> void:
 
 func reset_to_default() -> void:
 	var default: Dictionary = ggsManager.settings_data[str(setting_index)]["default"]
-	var event: InputEventKey = InputEventKey.new()
-	event.scancode = default["value"]
+	var event: InputEventWithModifiers
+	if default["value"] > 24:
+		event = InputEventKey.new()
+		event.scancode = default["value"]
+	else:
+		event = InputEventMouseButton.new()
+		event.button_index = default["value"]
 	_on_ConfirmPopup_confirmed(event)
 
 
@@ -43,17 +51,30 @@ func _on_pressed() -> void:
 	release_focus()
 
 
-func _on_ConfirmPopup_confirmed(event: InputEventKey) -> void:
+func _on_ConfirmPopup_confirmed(event: InputEventWithModifiers) -> void:
 	# Update save value
 	var current: Dictionary = ggsManager.settings_data[str(setting_index)]["current"]
-	current["value"] = event.scancode
+	if event is InputEventKey:
+		current["value"] = event.scancode
+	else:
+		current["value"] = event.button_index
 	ggsManager.save_settings_data()
 	
 	# Update display value
 	if icon != null:
-		icon.current_frame = event.scancode
+		if event is InputEventKey:
+			icon.current_frame = event.scancode
+		else:
+			icon.current_frame = event.button_index
 	else:
-		text = OS.get_scancode_string(event.scancode)
+		if event is InputEventKey:
+			text = OS.get_scancode_string(event.scancode)
+		else:
+			text = mouse_btn_names[str(event.button_index)]
 	
 	# Execute the logic script
 	script_instance.main(current)
+
+
+func _on_Button_button_down():
+	pass # Replace with function body.
