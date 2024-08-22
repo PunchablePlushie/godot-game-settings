a743a69294ae1e619307557b32297055f4eb1bf2
PunchablePlushie <64180414+PunchablePlushie@users.noreply.github.com>
2024-08-22 15:47:32 +0330

    Add subsections panel functionality

---
+++ addons/ggs/editor/core/core.gd
@@ -0,0 +1,5 @@
+@tool
+extends MarginContainer
+
+@export var icon_valid: Texture2D
+@export var icon_invalid: Texture2D
--- addons/ggs/editor/core/subsecs.gd
+++ addons/ggs/editor/core/subsecs.gd
@@ -4,27 +4,46 @@ extends PanelContainer
 @export_group("Nodes")
 @export var _Core: MarginContainer
 @export var _NewBtn: Button
+@export var _ResetBtn: Button
+@export var _SelectAllBtn: Button
+@export var _DeselectAllBtn: Button
 @export var _ReloadBtn: Button
 @export var _NewField: LineEdit
 @export var _List: ItemList
-@export var _Subsecs: PanelContainer
+@export var _DisabledLabel: Label
 
+var base_path: String
 var _item_name_is_valid: bool
-var _base_path: String
 
 
 func _ready() -> void:
-	_NewBtn.toggled.connect(_on_NewBtn_togged)
+	_NewBtn.toggled.connect(_on_NewBtn_toggled)
 	_NewField.text_changed.connect(_on_NewField_text_changed)
 	_NewField.text_submitted.connect(_on_NewField_text_submitted)
 	
 	_ReloadBtn.pressed.connect(_on_ReloadBtn_pressed)
+	_SelectAllBtn.pressed.connect(_on_SelectAllBtn_pressed)
+	_DeselectAllBtn.pressed.connect(_on_DeselectAllBtn_pressed)
 	_List.item_activated.connect(_on_List_item_activated)
 	_List.item_selected.connect(_on_List_item_selected)
+	_List.gui_input.connect(_on_List_gui_input)
 	
-	_base_path = GGS.Pref.path_settings
 	_NewField.hide()
-	_load_list()
+
+
+func set_panel_disabled(disabled: bool) -> void:
+	_NewBtn.disabled = disabled
+	_ResetBtn.disabled = disabled
+	_SelectAllBtn.disabled = disabled
+	_DeselectAllBtn.disabled = disabled
+	_ReloadBtn.disabled = disabled
+	_DisabledLabel.visible = disabled
+	_List.clear()
+	
+	if disabled:
+		_NewBtn.set_pressed_no_signal(false)
+		_NewField.hide()
+		_NewField.clear()
 
 
 #region Adding Items
@@ -34,11 +53,11 @@ func _set_item_name_valid(valid: bool) -> void:
 
 
 func _item_name_exists(item_name: String) -> bool:
-	var path: String = _base_path.path_join(item_name)
+	var path: String = base_path.path_join(item_name)
 	return DirAccess.dir_exists_absolute(path)
 
 
-func _on_NewBtn_togged(toggled_on: bool) -> void:
+func _on_NewBtn_toggled(toggled_on: bool) -> void:
 	_set_item_name_valid(false)
 	_NewField.visible = toggled_on
 	if toggled_on:
@@ -64,28 +83,37 @@ func _on_NewField_text_submitted(new_text: String) -> void:
 	
 	_NewField.clear()
 	
-	var path: String = _base_path.path_join(new_text)
+	var path: String = base_path.path_join(new_text)
 	DirAccess.make_dir_absolute(path)
 	EditorInterface.get_resource_filesystem().scan()
-	_load_list()
+	load_list()
+
 
 #endregion
 
 
 #region List
-func _load_list() -> void:
+func load_list() -> void:
 	_List.clear()
-	_Subsecs.set_panel_disabled(true)
 	
-	var items: PackedStringArray = DirAccess.get_directories_at(_base_path)
+	var items: PackedStringArray = DirAccess.get_directories_at(base_path)
 	for item: String in items:
 		var idx: int = _List.add_item(item)
-		_List.set_item_metadata(idx, _base_path.path_join(item))
+		_List.set_item_metadata(idx, base_path.path_join(item))
 
 
 func _on_ReloadBtn_pressed() -> void:
-	_load_list()
-	print("GGS - Reload Sections: Successful")
+	load_list()
+	print("GGS - Reload Subsections: Successful")
+
+
+func _on_SelectAllBtn_pressed() -> void:
+	for idx: int in _List.item_count:
+		_List.select(idx, false)
+
+
+func _on_DeselectAllBtn_pressed() -> void:
+	_List.deselect_all()
 
 
 func _on_List_item_activated(idx: int) -> void:
@@ -94,8 +122,17 @@ func _on_List_item_activated(idx: int) -> void:
 
 
 func _on_List_item_selected(idx: int) -> void:
-	_Subsecs.base_path = _List.get_item_metadata(idx)
-	_Subsecs.set_panel_disabled(false)
-	_Subsecs.load_list()
+	pass
+
+
+func _on_List_gui_input(event: InputEvent) -> void:
+	if (
+		event is not InputEventMouseButton
+		or event.button_index != MOUSE_BUTTON_RIGHT
+		or not event.is_pressed()
+	):
+		return
+	
+	_List.deselect_all()
 
 #endregion
